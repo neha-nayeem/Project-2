b3ab72cd125e2d22a42a8f21e2b1f6c089bb11d3
olivesun1213 <63970195+olivesun1213@users.noreply.github.com>
2020-09-26 13:55:55 -0400

    updates

--- static/js/temp.js
+++
@@ -1,7 +0,0 @@
-var queryurl = "http://127.0.0.1:5000/api"
-console.log("THIS IS TEMP.js")
-d3.json(queryurl).then(function (data) {
-  console.log(data);
-// Once we get a response, send the data.features object to the createFeatures function
-//  createFeatures(data);
-});
