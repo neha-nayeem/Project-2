ef6bda0325a42087b70e6d6d2260776cb6fe6e0b
Vexille437 <FengWang1991@users.noreply.github.com>
2020-09-27 16:29:27 -0400

    clean data update

--- app.py
+++ app.py
@@ -41,35 +41,52 @@ def get_data():
 def temp():
     return render_template("map.html")
 
-@app.route("/scatter")
-def scatter():
+@app.route("/charts")
+def charts():
 
     # Declare the collection
-    collection = mongo.db.all_crimes
+    collection = mongo.db.neighbourhood_summary
 
-    scatter_data=[]
+    chart_data=[]
 
     # Get only the data for robbery, assault and break and enter
     
-    for x in collection.find({'$or': [{'MCI': 'Robbery'}, {'MCI': 'Assault'}, {'MCI': 'Break and Enter'},{'MCI': 'bicycle theft'},{'MCI': 'Auto Theft'}]}, {"_id": 0}):
-        avg_age = x["Average age"]
-        income = x["  Average after-tax income of households in 2015 ($)"]
-        unemployment = x["Unemployment rate"]
-        neighbourhood = x["Neighbourhood Name"]
-        crime = x["MCI"]
+    # for x in collection.find({}, {"_id": 0}):
+        # avg_age = x["Average age"]
+        # income = x["household_income"]
+        # unemployment = x["Unemployment rate"]
+        # neighbourhood = x["Neighbourhood Name"]
+        # crime = x["MCI"]
+        # population_density=x["population_density"]
+        # population=x["Population"]
+        # number_of_crime=x["number_of_crime"]
+        
 
-        scatter = {
-            "Neighbourhood": neighbourhood,
-            "Avg_age": avg_age, 
-            "Avg_income": income, 
-            "Unemployment_rate": unemployment,
-            "Crime": crime
-        }
+        # data = {
+        #     "Neighbourhood": neighbourhood,
+        #     "Avg_age": avg_age, 
+        #     "Avg_income": income, 
+        #     "Unemployment_rate": unemployment,
+        #     "Crime": crime,
+        #     "Population": population,
+        #     "Population_density": population_density,
+        #     "Number_of_crime":number_of_crime
+        # }
+        # Get all results
+    results = collection.find({}, {"_id": 0})
 
-        scatter_data.append(scatter)  
+        # for loop to loop through each item in the database
+    for x in results:
+        # store each item (dict) in the list
+        chart_data.append(x)
 
     # Return a jsonified list of dictionaries
-    return jsonify(scatter_data)
+    return jsonify(chart_data)
+
+    #     chart_data.append(data)  
+
+    # # Return a jsonified list of dictionaries
+    # return jsonify(chart_data)
 
 if __name__ == "__main__":
     app.run(debug=True)
